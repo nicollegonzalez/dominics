4b8e3878afd38e08dcf0d49efab6bf5f41e32e4b
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2020-06-22 17:31:03 -0700

    Added photo slide show to homepage

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,4 +1,4 @@
-// config.js
-const config = { // this should be const instead if you're using ES6 standards
-  apiUrl : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAMEBfZ8dw1054JeBQs0oUn0PhiKT_brAI'
-}
+// // config.js
+// const config = { // this should be const instead if you're using ES6 standards
+//   apiUrl : 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAMEBfZ8dw1054JeBQs0oUn0PhiKT_brAI'
+// }
