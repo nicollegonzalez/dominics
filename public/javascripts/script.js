3c593c4d4c742aa5cd371a544c1e90458727d667
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2020-07-19 17:18:33 -0700

    Changed Covid banner content and also improved grid layout of locations div

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -62,29 +62,76 @@ mainContent.addEventListener('click', function(e){
 
 
 window.onload = function() {
-  navMenuLargeScreen.style.top = (informationHeader.clientHeight - window.scrollY) +'px';
   
+
+  navMenuLargeScreen.style.top = (informationHeader.clientHeight - window.scrollY) +'px';
   if( pathName == "/about" || pathName == "/careers"){
     logo.classList.add("scrolled");
     navBar.classList.toggle("scrolled");
   }
 }
 
-// if( pathName != "/about" && pathName != "/careers"){
-  window.onscroll = function(){
-    // console.log("Window is scrolling");
-    if(window.scrollY <= informationHeader.clientHeight){
-      navMenuLargeScreen.style.top = (informationHeader.clientHeight - window.scrollY) +'px';
-    }else {
-      navMenuLargeScreen.style.top = 0 +'px';
-    }
-    
-    if( pathName != "/about" && pathName != "/careers"){
-      navBar.classList.toggle("scrolled", window.scrollY > navBar.clientHeight + informationHeader.clientHeight || window.pageYOffset > navBar.clientHeight + informationHeader.clientHeight);
-      logo.classList.toggle("scrolled", window.scrollY > navBar.clientHeight + informationHeader.clientHeight || window.pageYOffset > navBar.clientHeight + informationHeader.clientHeight);
-    }
+window.onscroll = function(){
+  // console.log("Window is scrolling");
+  if(window.scrollY <= informationHeader.clientHeight){
+    navMenuLargeScreen.style.top = (informationHeader.clientHeight - window.scrollY) +'px';
+  }else {
+    navMenuLargeScreen.style.top = 0 +'px';
   }
-// }
+  
+  if( pathName != "/about" && pathName != "/careers"){
+    navBar.classList.toggle("scrolled", window.scrollY > navBar.clientHeight + informationHeader.clientHeight || window.pageYOffset > navBar.clientHeight + informationHeader.clientHeight);
+    logo.classList.toggle("scrolled", window.scrollY > navBar.clientHeight + informationHeader.clientHeight || window.pageYOffset > navBar.clientHeight + informationHeader.clientHeight);
+  }
+}
+
+function collapseSection(element) {
+  // get the height of the element's inner content, regardless of its actual size
+  var sectionHeight = element.scrollHeight;
+  
+  // temporarily disable all css transitions
+  var elementTransition = element.style.transition;
+  element.style.transition = '';
+  
+  // on the next frame (as soon as the previous style change has taken effect),
+  // explicitly set the element's height to its current pixel height, so we 
+  // aren't transitioning out of 'auto'
+  requestAnimationFrame(function() {
+    element.style.height = sectionHeight + 'px';
+    element.style.transition = elementTransition;
+    
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to height: 0
+    requestAnimationFrame(function() {
+      element.style.height = 0 + 'px';
+    });
+  });
+  
+  // mark the section as "currently collapsed"
+  element.setAttribute('data-collapsed', 'true');
+}
+
+function expandSection(element) {
+  // get the height of the element's inner content, regardless of its actual size
+  var sectionHeight = element.scrollHeight;
+  
+  // have the element transition to the height of its inner content
+  element.style.height = sectionHeight + 'px';
+
+  // when the next css transition finishes (which should be the one we just triggered)
+  element.addEventListener('transitionend', function(e) {
+    // remove this event listener so it only gets triggered once
+    element.removeEventListener('transitionend', arguments.callee);
+    
+    // remove "height" from the element's inline styles, so it can return to its initial value
+    element.style.height = null;
+  });
+  
+  // mark the section as "currently not collapsed"
+  element.setAttribute('data-collapsed', 'false');
+}
+
+
 
 
 
